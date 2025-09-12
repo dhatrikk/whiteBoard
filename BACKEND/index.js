88a840879bb5f526924ef601f6128e9c1d25cd62
Dhatrik <dhatrikkr@gmail.com>
2025-09-12 23:28:04 +0530

    cors resolved

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -1,30 +1,27 @@
 const connectToDB = require("./db.js");
+
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
 
-// CORS configuration
-app.use(cors({
-  origin: "https://white-board-gamma-eight.vercel.app",
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  credentials: true
-}));
-app.options("*", cors());
 
+app.use(cors());
 app.use(express.json());
 
 connectToDB();
 
-const userroutes = require("./routes/userroutes.js");
+const userroutes= require("./routes/userroutes.js");
 const canvasroutes = require("./routes/canvasroutes.js");
 
-app.use("/user", userroutes);
-app.use("/", canvasroutes);
+
+app.use("/user",userroutes);
+app.use("/",canvasroutes);
 
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
-  console.log(`${PORT} connected`);
+app.listen(PORT,()=>{
+console.log(`${PORT} connected`);
 });
+
